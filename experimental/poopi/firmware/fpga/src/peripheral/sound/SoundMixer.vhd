30c572246af9fdb54dd2466b336203507012e125
poopisan@gmail.com <poopisan@gmail.com@e3c020db-2bc8-ab18-1af0-91d435cfcb58>
2011-10-13 17:07:23 +0000

    Turned on SID emulation Scorpion stability is...questionable for turbo>x2 Refactored mixer to improve clock delay issue Added dedicated 16bit mixer input

--- experimental/poopi/firmware/fpga/src/peripheral/sound/SoundMixer.vhd
+++ experimental/poopi/firmware/fpga/src/peripheral/sound/SoundMixer.vhd
@@ -21,9 +21,9 @@ entity SoundMixer is
     WEIGHT_AUDIO2_AY_B : in unsigned(23 downto 0);-- sum should be <=1
     WEIGHT_AUDIO2_AY_C : in unsigned(23 downto 0);
 	 
-    I_AUDIO_AUX_1 : in  std_logic_vector(7 downto 0);--COVOX C1
-    I_AUDIO_AUX_2 : in  std_logic_vector(7 downto 0);--COVOX C2
-    I_AUDIO_AUX_3 : in  std_logic_vector(7 downto 0);--SID
+    I_AUDIO_AUX_1 : in  std_logic_vector( 7 downto 0);--COVOX C1
+    I_AUDIO_AUX_2 : in  std_logic_vector( 7 downto 0);--COVOX C2
+    I_AUDIO_AUX_3 : in  std_logic_vector(15 downto 0);--SID
 
     WEIGHT_AUDIO_AUX_1 : in  unsigned(23 downto 0);--COVOX C1
     WEIGHT_AUDIO_AUX_2 : in  unsigned(23 downto 0);--COVOX C2
@@ -60,64 +60,98 @@ architecture RTL of SoundMixer is
 		x"00000a",x"00000a",x"000009",x"000009",--18,19,1a,1b 
 		x"000009",x"000008",x"000008",x"000008" --1c,1d,1e,1f
     );
-  
+	 
+	signal in_ay1   :unsigned (23 downto 0);
+	signal in_ay2   :unsigned (23 downto 0);
+	signal in_aux1  :unsigned (23 downto 0);
+	signal in_aux2  :unsigned (23 downto 0);
+	signal in_aux3  :unsigned (23 downto 0);
+	signal in_beeper:unsigned (23 downto 0);
+	signal in_tape  :unsigned (23 downto 0);
+
+	signal weight_ay1   :unsigned (23 downto 0);
+	signal weight_ay2   :unsigned (23 downto 0);
+	signal weight_aux1  :unsigned (23 downto 0);
+	signal weight_aux2  :unsigned (23 downto 0);
+	signal weight_aux3  :unsigned (23 downto 0);
+	signal weight_beeper:unsigned (23 downto 0);
+	signal weight_tape  :unsigned (23 downto 0);
+
+	signal weight_sum:unsigned (23 downto 0);
+	
 begin
 
   process(clk)
   
-
-  variable weight_sum:unsigned (23 downto 0);
   variable output		:unsigned (23 downto 0); 
 
   begin
 	if clk'event and clk = '1' then
-		output  		:= x"000000";
-		weight_sum	:= x"0000FF";
-		
+		--output  		:= x"000000";
+		--weight_sum	:= x"0000FF";
+
 		if I_AY1_ENABLED = '1' then
-			weight_sum :=  weight_sum + WEIGHT_AUDIO1_AY_A + WEIGHT_AUDIO1_AY_B + WEIGHT_AUDIO1_AY_C;
-			output := output + unsigned(unsigned(x"0000" & I_AUDIO1_AY_A) * unsigned(WEIGHT_AUDIO1_AY_A))(23 downto 0) +
+			weight_ay1 <= WEIGHT_AUDIO1_AY_A + WEIGHT_AUDIO1_AY_B + WEIGHT_AUDIO1_AY_C;
+			in_ay1 <= unsigned(unsigned(x"0000" & I_AUDIO1_AY_A) * unsigned(WEIGHT_AUDIO1_AY_A))(23 downto 0) +
 									 unsigned(unsigned(x"0000" & I_AUDIO1_AY_B) * unsigned(WEIGHT_AUDIO1_AY_B))(23 downto 0) + 
 									 unsigned(unsigned(x"0000" & I_AUDIO1_AY_C) * unsigned(WEIGHT_AUDIO1_AY_C))(23 downto 0);
+		else
+			in_ay1     <= x"000000";
+			weight_ay1 <= x"000000";
 		end if;
 
 		if I_AY2_ENABLED = '1' then
-			weight_sum :=  weight_sum + WEIGHT_AUDIO2_AY_A + WEIGHT_AUDIO2_AY_B + WEIGHT_AUDIO2_AY_C;
-			output := output + unsigned(unsigned(x"0000" & I_AUDIO2_AY_A) * unsigned(WEIGHT_AUDIO2_AY_A))(23 downto 0) +
+			weight_ay1 <= WEIGHT_AUDIO2_AY_A + WEIGHT_AUDIO2_AY_B + WEIGHT_AUDIO2_AY_C;
+			in_ay2 <= unsigned(unsigned(x"0000" & I_AUDIO2_AY_A) * unsigned(WEIGHT_AUDIO2_AY_A))(23 downto 0) +
 									 unsigned(unsigned(x"0000" & I_AUDIO2_AY_B) * unsigned(WEIGHT_AUDIO2_AY_B))(23 downto 0) + 
 									 unsigned(unsigned(x"0000" & I_AUDIO2_AY_C) * unsigned(WEIGHT_AUDIO2_AY_C))(23 downto 0);
+		else
+			in_ay2     <= x"000000";
+			weight_ay2 <= x"000000";
 		end if;
 
 		if I_AUX1_ENABLED = '1' then
-			weight_sum :=  weight_sum + WEIGHT_AUDIO_AUX_1;
-			output := output + unsigned(unsigned(x"0000" & I_AUDIO_AUX_1) * unsigned(WEIGHT_AUDIO_AUX_1))(23 downto 0);
+			weight_aux1 <= WEIGHT_AUDIO_AUX_1;
+			in_aux1 <= unsigned(unsigned(x"0000" & I_AUDIO_AUX_1) * unsigned(WEIGHT_AUDIO_AUX_1))(23 downto 0);
+		else
+			in_aux1     <= x"000000";
+			weight_aux1 <= x"000000";
 		end if;
 		
 		if I_AUX2_ENABLED = '1' then
-			weight_sum :=  weight_sum + WEIGHT_AUDIO_AUX_2;
-			output := output + unsigned(unsigned(x"0000" & I_AUDIO_AUX_2) * unsigned(WEIGHT_AUDIO_AUX_2))(23 downto 0);
+			weight_aux2 <= WEIGHT_AUDIO_AUX_2;
+			in_aux2 <= unsigned(unsigned(x"0000" & I_AUDIO_AUX_2) * unsigned(WEIGHT_AUDIO_AUX_2))(23 downto 0);
+		else
+			in_aux2     <= x"000000";
+			weight_aux2 <= x"000000";
 		end if;
 		
 		if I_AUX3_ENABLED = '1' then
-			weight_sum :=  weight_sum + WEIGHT_AUDIO_AUX_3;
-			output := output + unsigned(unsigned(x"0000" & I_AUDIO_AUX_3) * unsigned(WEIGHT_AUDIO_AUX_3))(23 downto 0);
+			weight_aux3 <= WEIGHT_AUDIO_AUX_3;
+			in_aux3 <= unsigned(unsigned(x"00" & I_AUDIO_AUX_3) * unsigned(WEIGHT_AUDIO_AUX_3))(23 downto 0);
+		else
+			in_aux3     <= x"000000";
+			weight_aux3 <= x"000000";
 		end if;
 		
-		weight_sum :=  weight_sum + WEIGHT_AUDIO_BEEPER + WEIGHT_AUDIO_TAPE;
-		
 		if I_AUDIO_BEEPER = '1' then
-			output := output + unsigned(x"0000FF" * unsigned(WEIGHT_AUDIO_BEEPER))(23 downto 0);
+			in_beeper <= unsigned(x"0000FF" * unsigned(WEIGHT_AUDIO_BEEPER))(23 downto 0);
+		else
+			in_beeper <= x"000000";
 		end if;
 
 		if I_AUDIO_TAPE = '1' then
-			output := output + unsigned(x"0000FF" * unsigned(WEIGHT_AUDIO_TAPE))(23 downto 0);
+			in_tape <= unsigned(x"0000FF" * unsigned(WEIGHT_AUDIO_TAPE))(23 downto 0);
+		else
+			in_tape <= x"000000";
 		end if;
-		
-		O_AUDIO <= std_logic_vector(unsigned(output)(23 downto 8) * 
-				unsigned(dividerTable(to_integer(weight_sum(12 downto 8)))))(15 downto 0);--assuming input signals were all 8 bit only
-		
 	end if;
   end process;
 
+  weight_sum <= x"0000FF" + weight_ay1 + weight_ay2 + weight_aux1 + weight_aux2 + weight_aux3 + WEIGHT_AUDIO_BEEPER + WEIGHT_AUDIO_TAPE;
+
+  O_AUDIO <= std_logic_vector(
+				unsigned(in_ay1 + in_ay2 + in_aux1 + in_aux2 + in_aux3 + in_beeper + in_tape)(23 downto 8) * 
+				unsigned(dividerTable(to_integer(weight_sum(12 downto 8)))))(15 downto 0);--assuming input signals were all 8 bit only
 
 end architecture RTL;
